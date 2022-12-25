8ca1e30592b32508af0e8db57873c5f3576c537e
Chaeyeon Seo <120750451+ChaeYami@users.noreply.github.com>
2022-12-25 22:54:39 +0900

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,29 @@
+
+// 가입한 지 얼마 안 돼서 api키 활성화가 안 됨 나중에 다시 시도
+const API_KEY = '8bfb392399ce87807cdea209c7ce8878'
+
+
+function onGeoOk(position){
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    fetch(url).then(response =>response.json()).then(data=>{
+        const weather = document.getElementById("weather");
+        const city = document.getElementById("city");
+        const otherTemp = document.getElementById("other_temp");
+        
+
+        weather.innerText = `${data.main.temp}º , ${data.weather[0].main}`;
+        city.innerText =`${data.name}`;
+        otherTemp.innerText = `${data.main.temp_max}º / ${data.main.temp_min}º`;
+        
+    });
+}
+function onGeoError(){
+    alert("Can't find your location. No weather for you.")
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk,onGeoError);
+
+
+//temp-273.15
